976c291e834ad34cc86e07f669f91c1810b9d1f3
LivCodes <oliviareeddev@gmail.com>
2021-03-01 12:18:59 -0500

    remove

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     fetchStart();
     let form = document.querySelector(".add-form")
     form.addEventListener("submit", addToCollection)
-
+    
 });
 
 function fetchStart() {
@@ -17,7 +17,9 @@ function fetchStart() {
             <button class="btn">Remove</button>`;
 
             document.querySelector('#collection').append(div);
+            deleteFromCollection(div)
         })
+        
     })
 }
 
@@ -48,7 +50,28 @@ function addToCollection(e) {
     div.innerHTML = `<h2>${name}</h2>
     <img alt="${name}" src="${url}" class="avatar" />
     <button class="btn">Remove</button>`;
-
+    deleteFromCollection(div)
     document.querySelector('#collection').append(div);
     
+}
+
+function deleteFromCollection(card) {
+    let btn = card.children[2]
+    let name = card.children[0].innerText
+
+    btn.addEventListener("click", () => {
+        fetch(`http://localhost:3000/pops?name=${name}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data[0].id)
+            let options = {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            fetch(`http://localhost:3000/pops/${data[0].id}`, options)
+        })
+        card.remove()
+    })
 }
